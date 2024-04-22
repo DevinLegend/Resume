52c175a249f7f1dccb0c663b066702e9bd23b20e
Devin <devinsoto@gmail.com>
2024-04-21 21:27:17 -0700

    removed form submission

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwE5KRFGwTTMqF1B8tDUGuxycFMBqjF4XrX5XFqgC7VZAOjcY25zUTVuS_gsJ56rcU/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
-  
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        msg.innerHTML = "Message sent successfully"
-        setTimeout(function(){
-            msg.innerHTML = ""
-        },5000)
-        form.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
-})
-
 document.addEventListener('scroll', function() {
     const header = document.querySelector('.header-background');
     const scrollPosition = window.pageYOffset;
