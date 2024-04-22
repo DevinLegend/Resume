d61fde054305b635a21ffa1ad765bad58f221168
Devin <devinsoto@gmail.com>
2024-04-21 21:31:33 -0700

    js changed scrolling

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+let lastKnownScrollPosition = 0;
+let ticking = false;
+
 document.addEventListener('scroll', function() {
-    const header = document.querySelector('.header-background');
-    const scrollPosition = window.pageYOffset;
-    const opacityLevel = Math.min(0.8, scrollPosition / 500); // Adjust the 500 to control the fade speed
-    header.style.backgroundColor = `rgba(0, 0, 0, ${opacityLevel})`;
+    lastKnownScrollPosition = window.scrollY;
+
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+            const header = document.querySelector('.header-background');
+            const opacityLevel = Math.min(0.8, lastKnownScrollPosition / 500);
+            header.style.backgroundColor = `rgba(0, 0, 0, ${opacityLevel})`;
+            ticking = false;
+        });
+
+        ticking = true;
+    }
 });
